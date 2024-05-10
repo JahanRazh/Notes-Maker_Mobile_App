8bb1386045c5f82249549c2b9d91d70d969290e9
jahanRazh <jahanrazh@gmail.com>
2024-05-11 02:06:30 +0530

    modify

--- app/src/main/java/com/example/notesapp/AddNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/AddNoteActivity.kt
@@ -7,23 +7,33 @@ import android.widget.Toast
 import com.example.notesapp.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+    // Declare binding variable
     private lateinit var binding: ActivityAddNoteBinding
+    // Declare database helper variable
     private lateinit var db: NotesDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Inflate the layout using data binding
         binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize database helper
         db = NotesDatabaseHelper(this)
 
+        // Set OnClickListener for the save button
         binding.saveButton.setOnClickListener{
+            // Retrieve title and content from EditText fields
             val title = binding.titleEditText.text.toString()
             val content = binding.contentEditText.text.toString()
-            val note = Note(0,title,content)
+            // Create a new Note object
+            val note = Note(0, title, content)
+            // Insert the note into the database
             db.insertNote(note)
+            // Finish the activity
             finish()
+            // Display a toast to indicate that the note is saved
             Toast.makeText(this,"Note Saved",Toast.LENGTH_SHORT).show()
-
         }
     }
 }
