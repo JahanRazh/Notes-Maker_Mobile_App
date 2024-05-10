9b496901cb1ab9b301bc6bb87b79492004e8c974
jahanRazh <jahanrazh@gmail.com>
2024-05-11 00:14:21 +0530

    modify

--- app/src/main/java/com/example/notesapp/AddNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/AddNoteActivity.kt
@@ -1,11 +1,29 @@
 package com.example.notesapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.notesapp.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0,title,content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"Note Saved",Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
