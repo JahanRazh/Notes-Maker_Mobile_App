b25e30dc336b4f931bf9b5a71bea1b316c70f6db
jahanRazh <jahanrazh@gmail.com>
2024-05-12 00:31:28 +0530

    update

---
+++ app/src/main/java/com/example/notesapp/UpdateNoteActivity.kt
@@ -0,0 +1,41 @@
+package com.example.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.notesapp.databinding.ActivityUpdateNoteBinding
+
+
+class UpdateNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateNoteBinding
+    private lateinit var db: NotesDatabaseHelper
+    private var noteId:Int = -1
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+
+        noteId = intent.getIntExtra("note_id",-1)
+        if(noteId == -1){
+            finish()
+            return
+        }
+        val note = db.getNoteById(noteId)
+        binding.updatetitleEditText.setText(note.title)
+        binding.updatecontentEditText.setText(note.content)
+
+        binding.updateSaveButton.setOnClickListener{
+            val newTitle = binding.updatetitleEditText.text.toString()
+            val newContent = binding.updatecontentEditText.text.toString()
+            val updateNote = Note(noteId,newTitle,newContent)
+            db.updateNote(updateNote)
+            finish()
+            Toast.makeText(this, "Changes Saved", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
