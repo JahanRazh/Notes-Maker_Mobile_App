3ee8a0d912cd1fbc29b5c3595af89f98ee6f1016
jahanRazh <jahanrazh@gmail.com>
2024-05-11 11:44:49 +0530

    modify

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -7,10 +7,17 @@ import com.example.notesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NotesDatabaseHelper
+    private lateinit var  notesAdapter: NotesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+        notesAdapter =  NotesAdapter(db.getAllNotes(),this)
+
+
 
         binding.addbutton.setOnClickListener{
             val intent = Intent(this,AddNoteActivity::class.java)
